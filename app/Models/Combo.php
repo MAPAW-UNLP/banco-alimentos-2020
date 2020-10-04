218d95712193385be07365d256fb83b0fa82065f
sebastian <sebastianesg@gmail.com>
2020-10-04 20:31:18 -0300

    model

--- app/Models/Combo.php
+++ app/Models/Combo.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,4 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 class Combo extends Model
 {
     use HasFactory;
+    /**
+     * The primary key associated with the table.
+     *
+     * @var integer
+     */
+    protected $primaryKey = 'flight_id';
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'productos',
+        'stock',
+        'cant_org'
+    ];
 }
