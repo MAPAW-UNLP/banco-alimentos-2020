888010858dd842e40058c6f0faceecf4b28bd8ec
Nicolas Paparazzo <nico_95p@hotmail.com>
2020-11-03 16:37:28 -0300

    Mail

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -5,13 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Mail\RestartPassword;
 use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\UserController;
+use App\Models\User;
 
 class MailController extends Controller
 {
     public function resetPass(Request $request){
         $to = $request->email;
+
+            $user = User::where('email','=',$to)->first();
+        
+      
+           
+        
+        $newPass="resetpass";
+        $user['password']=$newPass;
+        $datos['pass'] = $newPass;
+        User::where('email','=',$to)->update($user->toArray());
         Mail::to($to)->send(new RestartPassword());
-        return view('emails.index');
+        //return response()->json($datos);
+        return view('emails.index',compact('datos',$datos));
     }
 }
 
--- resources/views/emails/envio-mail.blade.php
+++ resources/views/emails/envio-mail.blade.php
@@ -1,15 +1,14 @@
-
 <div class='header'>
-
 <img style="height:230px" class='img-fluid' src="https://bancoalimentario.org.ar/wp-content/uploads/2019/07/cropped-Imagen-para-pagina_page-0001.jpg">
 </div>
-
 
 <div class="general-container">
     <div class='right-container'>
         <div class="card-body">
         <h3>Recuperar contraseña</h3>
-            <h5>Para generar una nueva contraseña haga clic <a href="{{ url('/') }}">aqui</a></h5>
+
+            <h5>{{$pass ?? ''}}</h5>
+
             <h6>Banco de alimentos</h6>
         </div>
     </div>
