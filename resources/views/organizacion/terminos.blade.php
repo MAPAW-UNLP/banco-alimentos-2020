556e0022b430e50ff999295b47c993f6785d54dd
Nicolas Paparazzo <nico_95p@hotmail.com>
2020-11-05 20:44:16 -0300

    Registro de organizacion

--- resources/views/organizacion/terminos.blade.php
+++ resources/views/organizacion/terminos.blade.php
@@ -9,9 +9,12 @@
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
 
 <script>
-    function check(){
-        alert("Para continuar tenes que aceptar los terminos y condiciones");
-    }
+    function check_terminos(){ 
+   	if(document.getElementById("terminos").checked == false){
+        event.preventDefault();
+        alert('Debe aceptar los terminos y condiciones para continuar')
+       }
+}
 </script>
 
 <br>
@@ -19,15 +22,15 @@
 <div class='body-terminos'>
             <div class='body-request'>
                 <h3 style="color:white" aling="center">Terminos y condiciones para la solicitud de ingreso</h3>
-                <h4 style="color:white"> <input type="checkbox"> IMPORTANTE: DECLARO QUE LOS DATOS INGRESADOS A 
+                <h4 style="color:white"> <input type="checkbox" name="terminos" id="terminos"> IMPORTANTE: DECLARO QUE LOS DATOS INGRESADOS A 
                 CONTINUACIÓN CONSTITUYEN INFORMACIÓN VERDADERA Y COMPLETA, Y TIENEN CARÁCTER DE DECLARACIÓN JURADA</h4>
                 <div class='request-section'>
                     <div class='request-section-text'>
                     </div>
                     <div class='buttons-section'>
-                    <form id="myForm" action="{{ url('/') }}" method="post">
+                    <form name="formulario" id="terminos" action="{{ url('/registro') }}">
                         {{csrf_field()}}
-                        <button type="submit" onclick="javascript:check();">Continuar</button>
+                        <button type="submit" onclick="javascript:check_terminos();">Continuar</button>
                     </form>                  
                     </div>
                 </div>
