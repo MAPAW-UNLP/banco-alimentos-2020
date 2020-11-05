8a0fee496a8e626c3987cc77261129237698d661
EchuSosa <estersosaa@gmail.com>
2020-11-04 22:46:12 -0300

    fixed buttons colors (#48)
    Co-authored-by: echu-nan <ester.sosa@nan-labs.com>

--- resources/views/organizacione_show.blade.php
+++ resources/views/organizacione_show.blade.php
@@ -81,8 +81,8 @@
                         <p><b>Tarea que realizan:</b> {{$organizacione->tipo_servicio}}</p>
                     </div>
                     <div class='buttons-section'>
-                        <button type="cancel" class='reject-button' onclick="rechazar({{$organizacione->id}})" style="background-color:#ff0000;">Rechazar</button>
-                        <button type="submit" style="background-color:#f2994a;"><a href="{{ url('/aceptarOrg/'.$organizacione->id) }}">Aceptar</a> </button>                        
+                        <button type="cancel" class='reject-button' onclick="rechazar({{$organizacione->id}})" style="background-color: #dc3545;">Rechazar</button>
+                        <button type="submit" style="background-color: #103965"><a href="{{ url('/aceptarOrg/'.$organizacione->id) }}">Aceptar</a> </button>                        
                     </div>
                 </div>
                 </div>
