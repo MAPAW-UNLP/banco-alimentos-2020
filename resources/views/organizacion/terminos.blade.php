25c40deef132d683f26b00cee8d96049a68b8d25
echu-nan <ester.sosa@nan-labs.com>
2020-11-06 22:15:28 -0300

    fix continuar

--- resources/views/organizacion/terminos.blade.php
+++ resources/views/organizacion/terminos.blade.php
@@ -8,35 +8,31 @@
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
 
 <script>
-    function check_terminos(){ 
-   	if(document.getElementById("terminos").checked == false){
-        event.preventDefault();
-        alert('Debe aceptar los terminos y condiciones para continuar');
-       }
-}
+    function check_terminos(){
+        console.log('Holi') 
+        if(document.getElementById("terminos").checked == false){
+            event.preventDefault();
+            alert('Debe aceptar los terminos y condiciones para continuar');
+        }
+    }
 </script>
-
 <br>
-
 <div class='body-terminos'>
-            <div class='body-request'>
-                <h3 aling="center">Terminos y condiciones para la solicitud de ingreso</h3>
-                <h4><input type="checkbox"> IMPORTANTE: DECLARO QUE LOS DATOS INGRESADOS A 
-                CONTINUACIÓN CONSTITUYEN INFORMACIÓN VERDADERA Y COMPLETA, Y TIENEN CARÁCTER DE DECLARACIÓN JURADA</h4>
-                <div class='request-section'>                    
-                    <form id="myForm" action="{{ url('/') }}" method="post">
-                        {{csrf_field()}}
-                        <button type="submit" class="btn" onclick="javascript:check_terminos();" title="Continuar" style="background-color: #103965; color:#ffffff">
-                            Continuar
-                        </button>
-                    </form>                  
-                    </div>
-                </div>
-                </div>
-            </div>
+    <div class='body-request'>
+        <h3 aling="center">Terminos y condiciones para la solicitud de ingreso</h3>
+        <h4>
+            <input id="terminos" type="checkbox"> IMPORTANTE: DECLARO QUE LOS DATOS INGRESADOS A 
+            CONTINUACIÓN CONSTITUYEN INFORMACIÓN VERDADERA Y COMPLETA, Y TIENEN CARÁCTER DE DECLARACIÓN JURADA
+        </h4>
+        <div class='request-section'>                    
+            <form id="myForm" action="{{ url('/registro')}}" method="GET">
+                {{csrf_field()}}
+                <button type="submit" class="btn" onclick="javascript:check_terminos();" title="Continuar" style="background-color: #103965; color:#ffffff">
+                    Continuar
+                </button>
+            </form>                  
         </div>
     </div>
-</div>
 </div>
 <br>
 @include('components.footer')
