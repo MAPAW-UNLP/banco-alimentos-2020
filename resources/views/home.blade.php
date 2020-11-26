965445da5f21d06bbc1c64541b77aaadc7a45126
Nicolas Paparazzo <nico_95p@hotmail.com>
2020-11-26 00:45:30 -0300

    User disabled home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,14 @@
 @include('main')
 @include('components.header')
 @if(Auth::user()->estado==0)
- <h1>Pagaraplata</h1>
+    <div class="card" style=" margin: 70px 0px 60px; padding: 30px; align-items: center;">
+        <div class="card-body">
+            Su usuario no se encuentra habilitado. Comunicarse con el Banco aliementario.
+        </div>
+    </div>
 @else
-@include('components.nav')
-@include('components.home-body')
+    @include('components.nav')
+    @include('components.home-body')
+    
 @endif
 @include('components.footer')
