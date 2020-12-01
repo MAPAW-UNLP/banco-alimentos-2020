15a7db992ff1ea67e33a8862a3e2705a2092b1f9
chebasan <sebastian_sanchezg@hotmail.com>
2020-11-30 21:10:02 -0300

    vista solicitud (#82)
    Co-authored-by: sebastian <sebastianesg@gmail.com>

--- resources/views/estado-solicitud/index.blade.php
+++ resources/views/estado-solicitud/index.blade.php
@@ -26,21 +26,34 @@
           <h3>Solicitudes de Combos</h3>
           <div class='combos-body'>
             @if(count($pedidos) > 0)
-              <h4 class='font-white'>Combos</h4>
+              <table>
+              <tr>
+                <th>Turno</th>
+                <th colspan="2">Combos</th>
+                <th>estado</th>
+              </tr>
               @foreach($pedidos as $pedido)
                 <div class='solicitudes-tabla'>
+                <tr>
+                  <td>{{$pedido->turno->fechaHora}} - {{$pedido->turno->horario->nombre }}</td>
+                  <td>
+                  <table>
+                  
                 @foreach($pedido->combosPedidos as $comboPedido)
-                  <div class='request-section-text'>  
-                  
-                    <p><b>{{$comboPedido->combo->nombre}}</b></p>
-                  
-                  </div> 
-                  <div class='buttons-section'>
+                <tr>
+                    <td><p><b>{{$comboPedido->combo->nombre}}</b></p></td>
+                    <td><div class='buttons-section'>
                     <button type="submit" class='show-more'><a href="{{ url('/estadoSolicitud/solicitudCombos/'.$comboPedido->combo->id)}}">Ver mas</a></button>                     
-                  </div>
+                  </div></td>
+                  </tr>
                   @endforeach
+                
+                </table>
+                <td>{{$pedido->estado}}</td>
                 </div>
               @endforeach
+              </tr>
+              </table>
             @else
               <h5> No dispone de solicitudes de combos</h5>
             @endif              
