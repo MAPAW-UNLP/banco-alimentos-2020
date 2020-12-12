d779dc34c87b569cfcc3bef4249c13a29e563683
Nicolas Paparazzo <nico_95p@hotmail.com>
2020-12-12 11:16:58 -0300

    autocked terminos

--- resources/views/organizacion/terminos.blade.php
+++ resources/views/organizacion/terminos.blade.php
@@ -15,17 +15,21 @@
             alert('Debe aceptar los terminos y condiciones para continuar');
         }
     }
+    function autocheck(){
+        document.getElementById("terminus").checked = true;
+        document.getElementById("terminos").checked = true;
+    }
 </script>
 <br>
 <div class='body-terminos'>
     <div class='body-request'>
         <h3 aling="center" title="Solicitud de ingreso">Solicitud de ingreso</h3>
-        <h6 title="IMPORTANTE: DECLARO QUE LOS DATOS INGRESADOS A CONTINUACIÓN CONSTITUYEN INFORMACIÓN VERDADERA Y COMPLETA, Y TIENEN CARÁCTER DE DECLARACIÓN JURADA">
+        <h6 id="termino" name="termino" onclick="javascript:autocheck();" title="IMPORTANTE: DECLARO QUE LOS DATOS INGRESADOS A CONTINUACIÓN CONSTITUYEN INFORMACIÓN VERDADERA Y COMPLETA, Y TIENEN CARÁCTER DE DECLARACIÓN JURADA">
              IMPORTANTE: DECLARO QUE LOS DATOS INGRESADOS A
             CONTINUACIÓN CONSTITUYEN INFORMACIÓN VERDADERA Y COMPLETA, Y TIENEN CARÁCTER DE DECLARACIÓN JURADA
         </h6>
         <div>
-        <input id="terminos" type="checkbox" title="Seleccionar"> <b title="HE LEIDO Y ACEPTO LOS TERMINOS Y CONDICIONES">HE LEIDO Y ACEPTO LOS TERMINOS Y CONDICIONES</b>
+        <input id="terminos" name="terminos" type="checkbox" title="Seleccionar"> <b name="terminus" id="terminus" onclick="javascript:autocheck();" title="HE LEIDO Y ACEPTO LOS TERMINOS Y CONDICIONES">HE LEIDO Y ACEPTO LOS TERMINOS Y CONDICIONES</b>
         </div>
         <div class='request-section'>
             <form id="myForm" action="{{ url('/registro')}}" method="GET">
